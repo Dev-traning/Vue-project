6758ec9349c7ecb746c114895e7be96c67e0b7b1
Meet Bardoliya <meet.bardoliya@redjinni.com>
2022-03-17 16:15:05 +0530

    Changes in Axios file

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // axios.defaults.baseURL = 'https://api.garba.info/api/';
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 // axios.defaults.baseURL = 'https://api.restroworld.com/api/';
-axios.defaults.baseURL = 'http://restroworld.garba.info/api/';
+axios.defaults.baseURL = 'https://restroworld.garba.info/api/';
 
 axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token')
 
