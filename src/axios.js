a726726798a125686e4fbf5bdffcc4b660ad75e1
ParagDevac <76836391+ParagDevac@users.noreply.github.com>
2022-04-12 19:59:52 +0530

    Change URL to LIVE

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://api.restroworld.com/api/';
-axios.defaults.baseURL = 'https://api.restroworld.com/api/';
+ axios.defaults.baseURL = 'https://api.restroworld.com/api/';
+//axios.defaults.baseURL = 'https://uatapi.restroworld.com/api/';
 
 
 axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token')
