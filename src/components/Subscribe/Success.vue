87852c734b6e2a5ad392b75ed1e5704c29fcfb07
LAPTOP-QF8J1IKG\omche <omchevli8484@gmail.com>
2022-12-12 17:01:31 +0530

    123

--- src/components/Subscribe/Success.vue
+++ src/components/Subscribe/Success.vue
@@ -93,13 +93,13 @@ export default {
         });
     }
 
-    if (localStorage.getItem("expireSession")) {
-      setTimeout(() => {
-        localStorage.removeItem("expireSession");
-      }, 10000);
-    } else if (!localStorage.getItem("expireSession")) {
-      this.$router.push("/home");
-    }
+    // if (localStorage.getItem("expireSession")) {
+    //   setTimeout(() => {
+    //     localStorage.removeItem("expireSession");
+    //   }, 10000);
+    // } else if (!localStorage.getItem("expireSession")) {
+    //   this.$router.push("/home");
+    // }
 
     // .catch((error) => {
     //     this.errormas = error.response.data.message;
