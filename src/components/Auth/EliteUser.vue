40e86762f335008885c887dc3c2ce72ebba31de0
LAPTOP-QF8J1IKG\omche <omchevli8484@gmail.com>
2022-12-28 15:06:05 +0530

    changess28-12

--- src/components/Auth/EliteUser.vue
+++ src/components/Auth/EliteUser.vue
@@ -416,6 +416,7 @@ resend(){
                         this.plansData=result.data.data;
                         this.symbol= result.data.data[0].countries.currency_symbol
                         this.amount_payPrint=result.data.data[0].countries.pivot.amount
+                        localStorage.setItem("copondetails", JSON.stringify(res.data.data));
 
                         // // this.amount_pay =result.data.data[0].countries.pivot.amount
                         // this.amount_pay = 1
