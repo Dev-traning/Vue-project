7b792c00f9956a2ac1bb435f64fb679dadc6f527
om chevli <omchevli8484@gmail.com>
2023-11-18 14:15:10 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
  //axios.defaults.baseURL = 'https://api.garba.info/api/';
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
 // axios.defaults.baseURL = 'https://uatapi.restroworld.com/public/api/';
-axios.defaults.baseURL = 'https://uatapi.restroworld.com/api/';
+axios.defaults.baseURL = 'https://api.restroworld.com/api/';
+
 //  axios.defaults.baseURL = 'https://uatapi.restroworld.com/public/api/'
 // axios.defaults.baseURL = 'http://192.168.29.90:8000/api/';
 
